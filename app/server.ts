927ff0d07b65132375964d94328047d3657fae46
krishnadiamesso <krishnadiamesso@gmail.com>
2026-09-22 01:24:19 -0400

    Add `dev` script and improve validation in `/api/users` endpoint

--- app/server.ts
+++ app/server.ts
@@ -9,6 +9,18 @@ app.get('/health', (_req, res) => {
 
 app.post('/api/users', (req,res) => {
     const {name, email} = req.body;
+    if (!email) {
+        res.status(400).json({error: 'Email is required'});
+        return;
+    }
+
+    if (!name) {
+        res.status(400).json({error: 'Name is required'});
+        return;
+    }
+
+    console.log('User created', {name, email});
+
     res.status(201).json({id:1, name, email});
 })
 
