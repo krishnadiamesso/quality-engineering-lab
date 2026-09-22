76faf936c756d40546887165068e391a6452cdf5
krishnadiamesso <krishnadiamesso@gmail.com>
2026-09-21 23:30:09 -0400

    Configure development script and enable Playwright web server for health check test

--- tests/api/health.spec.ts
+++ tests/api/health.spec.ts
@@ -1,12 +1,11 @@
 import {test, expect} from '@playwright/test';
 
-test('get /health returns 200 status', async ({request}) => {
+test('GET /health returns 200 status', async ({request}) => {
     // Make http request
-    const response = await request.get('http://127.0.0.1:3000/health');
+    const response = await request.get('/health');
     const body = await response.json();
 
     // Make assertions
-    expect(response.ok()).toBeTruthy();
     expect(response.status()).toBe(200);
     expect(body.status).toBe('ok');
 })
